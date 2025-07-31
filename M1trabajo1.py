44d22a29d0405951ca797cea4905cff021e901a2
AndreuCi <carola.cfg@gmail.com>
2025-07-30 19:21:36 -0600

    Update M1trabajo1.py

--- M1trabajo1.py
+++ M1trabajo1.py
@@ -1,1 +1,26 @@
-print("hola")
+# Sorprende a tus compañerosi
+hobbies=["mi nombre:andreu ciuro fernandez","familia:tengo un hermano de 7 años","edad:11","fortalezas de programacion: celdas, random.randint, input ","debilidades en python:listas y dictionarios","amigos:jero, elias","juego fut en mi escuela, juago tenis","tengo un perro maltipoo llamdo milo"]
+import random
+while True:
+    mode="menu"
+    if mode=="menu":
+        a=input("que quieres print,add,remove")
+        mode=a
+    
+    if mode=="print":
+    
+        print(random.choice(hobbies))
+        mode="menu"
+    if mode=="add":
+        b=input("que hobbie quieres agregar")
+        hobbies.append(b)
+        mode="menu"
+    if mode=="remove":
+        c=input("que quieres remover")
+        if c in hobbies:
+            hobbies.remove(c)
+            mode="menu"
+        else:
+            print("no se encontro:(")
+            mode="menu"
+    
